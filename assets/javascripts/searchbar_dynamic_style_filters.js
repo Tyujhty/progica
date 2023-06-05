a04a13a75df6bc0d0a35f953e1149cfe3cafaa0f
Tyujhty <fannybaquey@gmail.com>
2023-06-05 08:32:07 +0200

    bug: correction du JS de la gestion des filtres

--- assets/javascripts/searchbar_dynamic_style_filters.js
+++ assets/javascripts/searchbar_dynamic_style_filters.js
@@ -1,6 +1,7 @@
 // Ajout d'une classe active sur la label des filtres à la sélection
 
 const labelsFilters = document.querySelectorAll('.labelFilter');
+const checkboxFilters = document.querySelectorAll('.checkboxFilter');
 
 labelsFilters.forEach(labelFilter => {
     labelFilter.addEventListener('mousedown', event => {
@@ -9,3 +10,13 @@ labelsFilters.forEach(labelFilter => {
         labelFilter.classList.toggle('activeFilter');
     })
 } )
+
+//Si on revient sur la recherche ou que l'on effectue une recherche depuis shelter
+checkboxFilters.forEach(checkboxFilter => {
+    if(checkboxFilter.checked === true) {
+
+        // récupère l'élément après la checkbox
+        const label = checkboxFilter.nextElementSibling;
+        label.classList.add('activeFilter');
+    }
+})
